0d978f28fe3b1946f018f85f70d97801642c9437
Shashank-Thattai <Shashankthattai@gmail.com>
2025-09-22 18:05:07 +0530

    HTML for my ToDo List

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,11 +3,12 @@ import React,{useState} from "react";
 
 
 function ToDOList(){
-const[tasks,setTasks] = useState([]);
+const[tasks,setTasks] = useState(["Eat Breakfast","Take a shower","Walk a dog"]);
 const[newTask,setNewTask]=useState("")
 
 function forInputChange(event)
 {
+    setNewTask(event.target.value)
 
 }
 function addTask(){
@@ -23,7 +24,35 @@ function moveTaskDown(index){
 
 }
 
-    return();
+    return(
+        <section>
+        <h1>To DO List</h1>
+
+        <div>
+            <input type="text" placeholder="Enter a task" value={newTask} onChange={forInputChange}/>
+            <button className="add-button" onClick={addTask}></button>
+
+        </div>
+
+        <ul>
+            {tasks.map((task,index)=>
+            <li key ={index}>
+                <span className="text">{task}</span>
+                <button className="delete-button" onClick={()=>deleteTask(index)}>Delete</button>
+                <button className="move-butto" onClick={()=>moveTaskUp(index)}>
+                    Up
+                </button>
+                <button className="move-butto" onClick={()=>moveTaskDown(index)}>
+                    Down
+                </button>
+            
+            </li>
+            )}
+        </ul>
+        </section>
+
+
+    );
 
 }
 export default ToDOlist
